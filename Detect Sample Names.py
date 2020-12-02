de671000bb12d4c9f3188aac7cc12690ddd7bda6
Anand Mahesh <manand881@gmail.com>
2020-12-02 09:51:48 +0530

    did some formatting

--- Detect Sample Names.py
+++ Detect Sample Names.py
@@ -1,37 +1,35 @@
 # this program has been purposed to find the names of the individuals in a chat.
 # this program works with only two people and not more than that.
 
+colon1_pos = None       # detect position of colon to identify the position of the name string
+dash1_pos = None        # detect position of colon to identify the position of the name string
+names = list()          # creating a an empty list to apprend names
+name1 = None            # name of the first person
+name2 = None            # name of the second person
+temp = None             # temporary variable
 
-colon1_pos  = None                                                  # detect position of colon to identify the position of the name string
-dash1_pos   = None                                                  # detect position of colon to identify the position of the name string
-names       = list()                                                # creating a an empty list to apprend names
-name1       = None                                                  # name of the first person
-name2       = None                                                  # name of the second person
-temp        = None                                                  # temporary variable
-
-f           = open("WhatsApp Chat with sample.txt",encoding="utf8") # opening sample whatsapp chat to detect names
+f = open("WhatsApp Chat with sample.txt", encoding="utf8")  # opening sample whatsapp chat to detect names
 
 for line in f:
 
-    reader=f.readline()                                             # assigning the line read to a variable
+    reader = f.readline()  # assigning the line read to a variable
 
-    if "end-to-end encryption. Tap for more info." in reader:       # skipping the unnecessary 
-        
+    if "end-to-end encryption. Tap for more info." in reader:  # skipping the unnecessary
+
         continue
 
-    if len(names)==2:                                               # ending the loop once the two names have been found                                        
-        
+    if len(names) == 2:  # ending the loop once the two names have been found
+
         break
 
-    colon1_pos=reader.find(":",13)                                  # names in whatsapp chats are inbetween a - & :
-    dash1_pos=reader.find("-")
-    temp=reader[dash1_pos:colon1_pos]                               # appending the name string to the temp variable
-    
-    if temp not in names and len(temp)>2:                           # if name isnt in the list and is longer than 2 chars then append
-        
+    colon1_pos = reader.find(":", 13)  # names in whatsapp chats are inbetween a - & :
+    dash1_pos = reader.find("-")
+    temp = reader[dash1_pos:colon1_pos]  # appending the name string to the temp variable
+
+    if temp not in names and len(temp) > 2:  # if name isnt in the list and is longer than 2 chars then append
+
         names.append(temp)
 
-
-name1=names[0]
-name2=names[1]
-print(name1,name2)                                          
+name1 = names[0]
+name2 = names[1]
+print(name1, name2)
--- Find Chats In Dir.py
+++ Find Chats In Dir.py
@@ -1,22 +1,19 @@
 # This program exists to detecct WhatsApp Chats in the directory of the program.
 
 
-from    os.path import isfile, join     # importing isFile and join to detect if the items in the directory are files 
-from    os      import listdir          # importing listdir to list items in the directory
-import  os                              # importing os to get current working direcotry
+from os.path import isfile, join        # importing isFile and join to detect if the items in the directory are files
+from os import listdir                  # importing listdir to list items in the directory
+import os                               # importing os to get current working direcotry
 
-
-chatfile_names  =   list()              # empty files to append chat file names
-mypath          =   os.getcwd()         # get current working direcotry path
-
+chatfile_names = list()                 # empty files to append chat file names
+mypath = os.getcwd()                    # get current working direcotry path
 
 for f in listdir(mypath):               # iterate through files in the current directory
-    
+
     if isfile(join(mypath, f)):         # getting path of the file in the current directory
-        
-        if ".txt" and "WhatsApp"in f:   # checking if they are WhatsApp chats
-            
+
+        if ".txt" and "WhatsApp" in f:  # checking if they are WhatsApp chats
+
             chatfile_names.append(f)    # append file names to list
 
-
-print("\n",chatfile_names)              # as it says
+print("\n", chatfile_names)             # as it says
